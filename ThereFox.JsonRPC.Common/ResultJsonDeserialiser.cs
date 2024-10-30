6d16848104febcfd7f25252d115923cd8a8191ad
ThereFox <ThereFox@tf.tf>
2024-10-30 09:29:10 +0400

    emergensy save

---
+++ ThereFox.JsonRPC.Common/ResultJsonDeserialiser.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFunctionalExtensions;
+using Newtonsoft.Json;
+
+namespace ThereFox.JsonRPC.Common;
+
+public static class ResultJsonDeserialiser
+{
+    public static Result<T> Deserialise<T>(string jsonString)
+        where T: class, new()
+    {
+        try
+        {
+            var result = JsonConvert.DeserializeObject<T>(jsonString);
+
+            if (result == null)
+            {
+                return Result.Failure<T>("Failed to deserialise request");
+            }
+            
+            return Result.Success(result);
+        }
+        catch (Exception e)
+        {
+            return Result.Failure<T>("Invalid format");
+        }
+    }
+}
