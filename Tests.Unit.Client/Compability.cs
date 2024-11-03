daef47a467cf156d4a6c197163146cd6b86ae2a7
ThereFox <ThereFox@tf.tf>
2024-11-03 17:42:29 +0400

    Add some tests for requestBuilder

---
+++ Tests.Unit.Client/Compability.cs
@@ -0,0 +1,63 @@
+﻿using ThereFox.JsonRPC;
+using ThereFox.JsonRPC.Core.Client;
+
+namespace Tests.Unit.Client;
+
+public class Compability
+{
+    private readonly JsonRPCRequestBuilder _sut;
+    private readonly RequestParser _corrector;
+
+    public Compability()
+    {
+        _sut = new JsonRPCRequestBuilder();
+        _corrector = new RequestParser();
+    }
+
+    [Fact]
+    public void Client_ParserReadRequest_WithoutParams__ShoultSucsesfully()
+    {
+        var method = "MethodName";
+
+        var requestJson = _sut.CreateRequest(method, new List<object>());
+        var requestContent = _corrector.Parse(requestJson);
+        
+        Assert.True(requestContent.IsSuccess);
+        Assert.Empty(requestContent.Value.Arguments);
+        Assert.Equal(method, requestContent.Value.ActionName);
+    }
+    
+    [Fact]
+    public void Client_ParserReadRequest_WithOneArguments__ShoultSucsesfully()
+    {
+        var method = "MethodName";
+
+        var requestJson = _sut.CreateRequest(method, new List<object>() { "1" });
+        var requestContent = _corrector.Parse(requestJson);
+        
+        Assert.True(requestContent.IsSuccess);
+        Assert.NotEmpty(requestContent.Value.Arguments);
+        Assert.Equal(1, requestContent.Value.Arguments.Count());
+        Assert.Equal("1", requestContent.Value.Arguments.First().Value);
+        Assert.Equal(method, requestContent.Value.ActionName);
+    }
+    
+    [Fact]
+    public void Client_ParserReadRequest_WithSomeArguments__ShoultSucsesfully()
+    {
+        var method = "MethodName";
+
+        var requestJson = _sut.CreateRequest(method, new List<object>() { "1", 3, "3" });
+        var requestContent = _corrector.Parse(requestJson);
+        
+        Assert.True(requestContent.IsSuccess);
+        Assert.NotEmpty(requestContent.Value.Arguments);
+        Assert.Equal(3, requestContent.Value.Arguments.Count());
+        Assert.Equal("1", requestContent.Value.Arguments[0].Value);
+        Assert.Equal((long)3, requestContent.Value.Arguments[1].Value);
+        Assert.Equal("3", requestContent.Value.Arguments[2].Value);
+        Assert.Equal(method, requestContent.Value.ActionName);
+    }
+    
+    
+}
